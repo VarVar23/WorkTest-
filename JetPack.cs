43abecbe017bbbdec64f69898056d80f2b54d39c
NooB <oleg.nakhaevgame@mail.ru>
2021-06-10 04:01:10 +0300

    Add JetPack

---
+++ JetPack.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetPack : MonoBehaviour
+{
+    [HideInInspector] public bool CheckMouseClamp;
+    [SerializeField] private float SpeedMove;
+    [SerializeField] private float SpeedRotate;
+    
+    private Transform _thisTransform;
+
+    private Vector2 _mousePosition;
+    private Vector2 _direction;
+    private Vector2 _currentDirection;
+    private Vector2 _myTransformPosition;
+
+    void Start()
+    {
+        _thisTransform = this.transform;
+        CheckMouseClamp = false;
+    }
+
+
+    void Update()
+    {
+        MouseClamp();
+
+        if(CheckMouseClamp)
+        {
+            FlyJetPack();
+        }
+    }
+
+    private void MouseClamp()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            CheckMouseClamp = true;
+        }
+        else if (Input.GetMouseButtonUp(0))
+        {
+            CheckMouseClamp = false;
+        }  
+    }
+
+    private void FlyJetPack()
+    {
+        _mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        _myTransformPosition = _thisTransform.position;
+
+        _direction = _mousePosition - _myTransformPosition;
+        _direction.Normalize();
+
+        _currentDirection = Vector2.Lerp(_currentDirection, _direction, SpeedRotate * Time.deltaTime);
+        _thisTransform.up = _currentDirection;
+
+        if (Vector2.Distance(_myTransformPosition, _mousePosition) > 1f)
+        {
+            transform.Translate(Vector3.up * SpeedMove * Time.deltaTime);
+        }
+    }
+}
+
+
---
+++ JetPackActivate.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JetPackActivate : MonoBehaviour
+{
+    private GameObject _player;
+    private Quaternion _playerStartRotation;
+    private bool _trigger;
+
+    private void Start()
+    {
+        _player = FindObjectOfType<Player>().gameObject;
+        _playerStartRotation = _player.transform.rotation;
+        _trigger = true;
+    }
+
+    public void ButtonActivateJetPack()
+    {
+        if(_trigger)
+        {
+            _player.GetComponent<Player>().enabled = false;
+            _player.GetComponent<Controller2D>().enabled = false;
+            _player.GetComponent<JetPack>().enabled = true;
+            _player.GetComponent<JetPack>().CheckMouseClamp = false;
+
+            _trigger = false;
+        }
+        else
+        {
+            _player.GetComponent<Player>().enabled = true;
+            _player.GetComponent<Controller2D>().enabled = true;
+            _player.GetComponent<JetPack>().enabled = false;
+            _player.transform.rotation = _playerStartRotation;
+
+            _trigger = true;
+        }
+    }
+}
